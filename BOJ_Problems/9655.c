5ec15e577d73a97cbe66fb155db8369c08d942a5
hye-won <105777703+hye1w@users.noreply.github.com>
2024-02-01 11:28:31 +0900

    Create 9655.c

---
+++ BOJ_Problems/9655.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    if (N % 2 == 0) {
+        printf("CY\n");
+    } else {
+        printf("SK\n");
+    }
+
+    return 0;
+}
