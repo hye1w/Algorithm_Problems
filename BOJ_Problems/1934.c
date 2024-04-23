be51a5533b6abfd2e963065eecb2a5962d6832e3
hye-won <105777703+hye1w@users.noreply.github.com>
2024-04-23 16:35:29 +0900

    Create 1934.c

---
+++ BOJ_Problems/1934.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = a % b;
+        a = b;
+        b = temp;
+    }
+    return a;
+}
+
+int lcm(int a, int b) {
+    return a * b / gcd(a, b);
+}
+
+int main() {
+    int T;
+    scanf("%d", &T);
+
+    while (T--) {
+        int A, B;
+        scanf("%d %d", &A, &B);
+        
+        printf("%d\n", lcm(A, B));
+    }
+
+    return 0;
+}
