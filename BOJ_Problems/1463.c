85ebd6372de1da1a22b50a541a874d9812eeb626
hye-won <105777703+hye1w@users.noreply.github.com>
2024-04-30 22:51:29 +0900

    Create 1463.c

---
+++ BOJ_Problems/1463.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int min(int a, int b) {
+    return a < b ? a : b;
+}
+
+int minOperations(int n) {
+    int dp[n + 1];
+    dp[1] = 0; 
+
+    for (int i = 2; i <= n; i++) {
+        dp[i] = dp[i - 1] + 1;  
+
+        if (i % 2 == 0)
+            dp[i] = min(dp[i], dp[i / 2] + 1);  
+        if (i % 3 == 0)
+            dp[i] = min(dp[i], dp[i / 3] + 1); 
+    }
+
+    return dp[n];
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    printf("%d\n", minOperations(n));
+
+    return 0;
+}
