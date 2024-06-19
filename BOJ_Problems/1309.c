830a41b01264c9dc91f416be2cf41615e418aa37
hye-won <105777703+hye1w@users.noreply.github.com>
2024-06-19 22:04:13 +0900

    Create 1309.c

---
+++ BOJ_Problems/1309.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#define MOD 9901
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    int dp[N+1][3];
+ 
+    dp[1][0] = 1;
+    dp[1][1] = 1; 
+    dp[1][2] = 1; 
+
+    for (int i = 2; i <= N; i++) {
+        dp[i][0] = (dp[i-1][0] + dp[i-1][1] + dp[i-1][2]) % MOD; 
+        dp[i][1] = (dp[i-1][0] + dp[i-1][2]) % MOD; 
+        dp[i][2] = (dp[i-1][0] + dp[i-1][1]) % MOD;
+    }
+
+    int result = (dp[N][0] + dp[N][1] + dp[N][2]) % MOD;
+    printf("%d\n", result);
+
+    return 0;
+}
